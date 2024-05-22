987970135a5fb324e398915050ca17dd360de8b8
Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>
2024-05-22 17:17:41 +0530

    Fixed app initialisation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,15 @@
 const express=require("express");
 const cors=require("cors");
 
+const app=express();
+
 app.use(cors());
 app.use(express.json());
 
 const userRouter=require("./user")
 const accountRouter=require("./accounts")
 
-const app=express();
+
 
 const router=express.Router();
 
