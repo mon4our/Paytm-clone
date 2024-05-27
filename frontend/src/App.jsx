b0a7d8bc7b763180a512321d9a9c45a6da845e7e
Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>
2024-05-27 19:25:00 +0530

    Integrated fe and be
    90% complete
    Only finishing touches left

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/signUp";
+import { Signin } from "./pages/signIn";
+import { Dashboard } from "./pages/dashboard";
+import { SendMoney } from "./pages/sendMoney";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
